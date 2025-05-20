aa26852d104340ac158fb3b92cfb203ff9eab311
Yevgen <39760876+y3v63n@users.noreply.github.com>
2025-05-20 19:28:50 +0400

    Update start_miner.sh

--- start_miner.sh
+++ start_miner.sh
@@ -11,6 +11,8 @@ mkdir -p "$LOG_DIR"
 for i in $(seq 0 $((NUM_MINERS - 1))); do
     MINER_DIR="$BASE_DIR/miner_$i"
     mkdir -p "$MINER_DIR"
+    
+SOCKET_PATH="$MINER_DIR/nockchain_npc_$i.sock"
 
     # Use symlink to binary for easy updates
     ln -sf "$(realpath $NCK_BINARY)" "$MINER_DIR/nockchain"
@@ -19,7 +21,8 @@ for i in $(seq 0 $((NUM_MINERS - 1))); do
 
     "$MINER_DIR/nockchain" --mine \
         --mining-pubkey 2wKyknRVrBw1jvPFMib4pk5ENBjopFGyGeSXcSp6QBZz38YCtaxETNGuN8ijAmomsjUD1hpWPyu9TeC1GkcojoszCnWntu3mSxjni5r9M2QYft7Jf9QTk4W8q1nVarkB9JjB \
-        --btc-node-url https://bitcoin-mainnet.core.chainstack.com/69bfa311c13f64698a539bb0f48153eb > "$LOG_DIR/miner_$i.log" 2>&1 &
+        --btc-node-url https://bitcoin-mainnet.core.chainstack.com/69bfa311c13f64698a539bb0f48153eb \
+        --npc-socket "$SOCKET_PATH" > "$LOG_DIR/miner_$i.log" 2>&1 &
 done
 
 echo "✅ Launched $NUM_MINERS miners on macOS."
