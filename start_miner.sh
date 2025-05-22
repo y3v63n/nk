2208634ccdb601e0224fe33fd85ef0b7702b0721
Yevgen <39760876+y3v63n@users.noreply.github.com>
2025-05-22 06:25:33 +0400

    Update start_miner.sh

--- start_miner.sh
+++ start_miner.sh
@@ -22,16 +22,16 @@ SOCKET_PATH="$MINER_DIR/nockchain_npc_$i.sock"
     "$MINER_DIR/nockchain" --mine \
         --mining-pubkey 3u71QFPLDS8WBzkk2vSCEQW72LBTdE4rxmqf1hKMfini9iUPHYb9kuakkcMDsToZUsHP7SXBkQTwFRLgWZDHoFDVRBRS9vDjVZ23qujtQ14m2DeRnL3tJj364wy5tNPSZJ9f \
         --btc-node-url https://bitcoin-mainnet.core.chainstack.com/69bfa311c13f64698a539bb0f48153eb \
-        --peer "dnsaddr=/ip4/34.35.75.234/udp/30000/quic-v1" \
-        --peer "dnsaddr=/ip4/34.176.41.23/udp/30000/quic-v1" \
-        --peer "dnsaddr=/ip4/34.16.237.144/udp/30000/quic-v1" \
-        --peer "dnsaddr=/ip4/34.85.34.153/udp/30000/quic-v1" \
-        --peer "dnsaddr=/ip4/34.95.155.151/udp/30000/quic-v1" \
-        --peer "dnsaddr=/ip4/34.97.242.48/udp/30000/quic-v1" \
-        --peer "dnsaddr=/ip4/34.162.206.28/udp/30000/quic-v1" \
-        --peer "dnsaddr=/ip4/34.174.22.166/udp/30000/quic-v1" \
-        --peer "dnsaddr=/ip4/34.129.248.106/udp/30000/quic-v1" \
-        --peer "dnsaddr=/ip4/34.18.98.38/udp/30000/quic-v1" \
+        --peer "/ip4/34.35.75.234/udp/30000/quic-v1" \
+        --peer "/ip4/34.176.41.23/udp/30000/quic-v1" \
+        --peer "/ip4/34.16.237.144/udp/30000/quic-v1" \
+        --peer "/ip4/34.85.34.153/udp/30000/quic-v1" \
+        --peer "/ip4/34.95.155.151/udp/30000/quic-v1" \
+        --peer "/ip4/34.97.242.48/udp/30000/quic-v1" \
+        --peer "/ip4/34.162.206.28/udp/30000/quic-v1" \
+        --peer "/ip4/34.174.22.166/udp/30000/quic-v1" \
+        --peer "/ip4/34.129.248.106/udp/30000/quic-v1" \
+        --peer "/ip4/34.18.98.38/udp/30000/quic-v1" \
         --npc-socket "$SOCKET_PATH" > "$LOG_DIR/miner_$i.log" 2>&1 &
 done
 
